0004d522bf6c5fafc4585ea9ac80f7f4662b5a76
Paulo Franklin <paulofsnunes@gmail.com>
2022-04-05 21:14:01 -0300

    third commit

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,25 +1,38 @@
 <template>
     <section class="product-container">
-        <div v-for="produto in produtos" :key="produto.id">
-            <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
-            <p class="price">{{produto.preco}}</p>
-            <h2 class="title">{{produto.nome}}</h2>
-            <p class="description">{{produto.descricao}}</p>
+        <div v-if="produtos && produtos.length" class="products">
+            <div class="product" v-for="(produto, index) in produtos" :key="index">
+                <router-link to="/">
+                    <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
+                    <p class="price">{{produto.preco}}</p>
+                    <h2 class="title">{{produto.nome}}</h2>
+                    <p class="description">{{produto.descricao}}</p>
+                </router-link>
+            </div>
+            <products-page :productsTotal="productsTotal" :productsForPage="productsForPage"/>
+        </div>
+        <div v-else-if="produtos && produtos.length === 0">
+            <p class="no-results">Busca sem resultados ☹️ Tente buscar outro termo.</p>
         </div>
     </section>
 </template>
 
 <script>
 
+import ProductsPage from "@/components/ProductsPage.vue"
 import { api } from "@/services/services.js"
 import { serialize } from "@/helpers/helpers.js"
 
 export default {
     name: "ProductsList",
+    components: {
+        ProductsPage
+    },
     data() {
         return {
             produtos: null,
-            productsForPage: 9
+            productsForPage: 3,
+            productsTotal: 0
         }
     },
     computed: {
@@ -32,6 +45,7 @@ export default {
         getProducts() {
             api.get(this.url)
             .then(r => {
+                this.productsTotal = Number(r.headers["x-total-count"])
                 console.log(r)
                 this.produtos = r.data
             })
@@ -49,5 +63,48 @@ export default {
 </script>
 
 <style scoped>
+.product-container {
+    max-width: 1000px;
+    margin: 0 auto;
+}
 
+.products {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 30px;
+    margin: 30px;
+}
+
+.product {
+    box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+    padding: 10px;
+    background: #fff;
+    border-radius: 4px;
+    transition: all 0.2s;
+}
+
+.product:hover {
+    box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+    transform: scale(1.1);
+    position: relative;
+    z-index: 1;
+}
+
+.product img {
+    border-radius: 4px;
+    margin-bottom: 20px;
+}
+
+.title {
+    margin-bottom: 10px;
+}
+
+.price {
+    color: #e80;
+    font-weight: bold;
+}
+
+.no-results {
+    text-align: center;
+}
 </style>
--- src/components/ProductsSearch.vue
+++ src/components/ProductsSearch.vue
@@ -1,6 +1,6 @@
 <template>
     <form>
-        <input name="search" id="search" type="text" v-model="search">
+        <input name="search" id="search" type="text" v-model="search" placeholder="Buscar...">
         <input type="submit" id="magnifier" value="Buscar" @click.prevent="searchProducts">
     </form>
 </template>
@@ -22,5 +22,33 @@ export default {
 </script>
 
 <style scoped>
+form {
+    max-width: 600px;
+    margin: 30px auto 60px auto;
+    position: relative;
+}
 
+#search {
+    width: 100%;
+    padding: 20px;
+    border: none;
+}
+
+#search:focus, #search:hover {
+    transform: scale(1.1);
+}
+
+#magnifier {
+    width: 62px;
+    height: 62px;
+    background: url(../assets/search.svg) no-repeat center center;
+    text-indent: -150px;
+    border: none;
+    cursor: pointer;
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    box-shadow: none;
+
+}
 </style>
